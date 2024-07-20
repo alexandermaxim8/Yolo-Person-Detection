b87172316938d8067d60812b937001b3365181c2
alexandermaxim8 <143409662+alexandermaxim8@users.noreply.github.com>
2024-07-21 00:22:14 +0700

    Add files via upload

---
+++ objcount_yolo.py
@@ -0,0 +1,34 @@
+import cv2
+from ultralytics import YOLO, solutions
+
+model = YOLO("run/run/runs/detect/train2/weights/best.pt")
+cap = cv2.VideoCapture("background video _ people _ walking _.mp4")
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+line_points = [(0, h/2), (w, h/2)]
+
+video_writer = cv2.VideoWriter("object_counting_output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps/2, (w, h))
+
+# Init Object Counter
+counter = solutions.ObjectCounter(
+    view_img=True,
+    reg_pts=line_points,
+    count_reg_color=(255, 0, 0),
+    names=model.names,
+    draw_tracks=True,
+    line_thickness=2,
+)
+
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, tracker='bytetrack.yaml', conf=0.6, show=True)
+
+    im0 = counter.start_counting(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
